4d9a5c080d83a9ac803613873df2aa7a233135da
Vishnuvardhan Reddy <101919480+G-Vishnu11@users.noreply.github.com>
2022-04-27 10:04:16 +0530

    Final Project Files v2.0

---
+++ src/pypm/simulation.py
@@ -0,0 +1,167 @@
+from pypm import data_io
+from pypm.portfolio import PortfolioHistory, Position, Symbol
+
+from typing import Tuple, List, Dict, Callable, Iterable
+import pandas as pd
+from collections import OrderedDict
+
+class SimpleSimulator(object):
+
+    def __init__(self, initial_cash: float=100000, max_active_positions: int=5, percent_slippage: float=0.0005, trade_fee: float=1):
+        # Set simulation parameters
+
+        self.initial_cash = self.cash = initial_cash
+
+        # Maximum number of different assets that can be held simultaneously
+        self.max_active_positions: int = max_active_positions
+
+        # The percentage difference between closing price and fill price for the order placed
+        self.percent_slippage = percent_slippage
+
+        # The fixed fee in order to open a position in rupee terms
+        self.trade_fee = trade_fee
+
+        # Keep track of live trades
+        self.active_positions_by_symbol: Dict[Symbol, Position] = OrderedDict()
+
+        # Keep track of portfolio history like cash, equity, and positions
+        self.portfolio_history = PortfolioHistory()
+
+    @property
+    def active_positions_count(self):
+        return len(self.active_positions_by_symbol)
+
+    @property
+    def free_position_slots(self):
+        return self.max_active_positions - self.active_positions_count
+
+    @property
+    def active_symbols(self) -> List[Symbol]:
+        return list(self.active_positions_by_symbol.keys())
+
+    def print_initial_parameters(self):
+        s = f'Initial Cash: ₹{self.initial_cash} \n' \
+            f'Maximum Number of Assets: {self.max_active_positions}\n'
+        print(s)
+        return s
+
+    @staticmethod
+    def make_tuple_lookup(columns) -> Callable[[str, str], int]:
+        tuple_lookup: Dict[Tuple[str, str], int] = { 
+            col: i + 1 for i, col in enumerate(columns) 
+        }
+        return lambda symbol, metric: tuple_lookup[(symbol, metric)]
+
+    @staticmethod
+    def make_all_valid_lookup(_idx: Callable):
+        return lambda row, symbol: (
+            not pd.isna(row[_idx(symbol, 'pref')]) and \
+            not pd.isna(row[_idx(symbol, 'signal')]) and \
+            not pd.isna(row[_idx(symbol, 'price')])
+        )
+
+    def buy_to_open(self, symbol, date, price):
+        cash_available = self.cash - self.trade_fee
+        cash_to_spend = cash_available / self.free_position_slots
+        
+        purchase_price = (1 + self.percent_slippage) * price
+        shares = cash_to_spend / purchase_price
+
+        self.cash -= cash_to_spend + self.trade_fee
+        assert self.cash >= 0, 'Spent cash you do not have.'
+        self.portfolio_history.record_cash(date, self.cash)   
+
+        positions_by_symbol = self.active_positions_by_symbol
+        assert not symbol in positions_by_symbol, 'Symbol already in portfolio.'        
+        position = Position(symbol, date, purchase_price, shares)
+        positions_by_symbol[symbol] = position
+
+    def sell_to_close(self, symbol, date, price):
+
+        # Exit the position
+        positions_by_symbol = self.active_positions_by_symbol
+        position = positions_by_symbol[symbol]
+        position.exit(date, price)
+
+        # Receive the cash
+        sale_value = position.last_value * (1 - self.percent_slippage)
+        self.cash += sale_value
+        self.portfolio_history.record_cash(date, self.cash)
+
+        # Record in portfolio history
+        self.portfolio_history.add_to_history(position)
+        del positions_by_symbol[symbol]
+
+    @staticmethod
+    def _assert_equal_columns(*args: Iterable[pd.DataFrame]):
+        column_names = set(args[0].columns.values)
+        for arg in args[1:]:
+            assert set(arg.columns.values) == column_names, \
+                'Found unequal column names in input dataframes.'
+
+    def simulate(self, price: pd.DataFrame, signal: pd.DataFrame, preference: pd.DataFrame):
+
+        self._assert_equal_columns(price, signal, preference)
+        df = data_io.concatenate_metrics({
+            'price': price,
+            'signal': signal,
+            'pref': preference,
+        })
+
+        all_symbols = list(set(price.columns.values))
+
+        _idx = self.make_tuple_lookup(df.columns)
+        _all_valid = self.make_all_valid_lookup(_idx)
+
+        active_positions_by_symbol = self.active_positions_by_symbol
+        max_active_positions = self.max_active_positions
+
+        for row in df.itertuples():
+
+            date = row[0]
+
+            symbols: List[str] = [s for s in all_symbols if _all_valid(row, s)]
+
+            _active = self.active_symbols
+            to_exit = [s for s in _active if row[_idx(s, 'signal')] == -1]
+            for s in to_exit:
+                sell_price = row[_idx(s, 'price')]
+                self.sell_to_close(s, date, sell_price)
+
+            to_buy = [
+                s for s in symbols if \
+                    row[_idx(s, 'signal')] == 1 and \
+                    not s in active_positions_by_symbol
+            ]
+            to_buy.sort(key=lambda s: row[_idx(s, 'pref')], reverse=True)
+            to_buy = to_buy[:max_active_positions]
+
+            for s in to_buy:
+                buy_price = row[_idx(s, 'price')]
+                buy_preference = row[_idx(s, 'pref')]
+
+                if self.active_positions_count < max_active_positions:
+                    self.buy_to_open(s, date, buy_price)
+                    continue
+
+                _active = self.active_symbols
+                active_prefs = [(s, row[_idx(s, 'pref')]) for s in _active]
+
+                _min = min(active_prefs, key=lambda k: k[1])
+                min_active_symbol, min_active_preference = _min
+
+                if min_active_preference < buy_preference:
+                    sell_price = row[_idx(min_active_symbol, 'price')]
+                    self.sell_to_close(min_active_symbol, date, sell_price)
+                    self.buy_to_open(s, date, buy_price)
+
+            for s in self.active_symbols:
+                price = row[_idx(s, 'price')]
+                position = active_positions_by_symbol[s]
+                position.record_price_update(date, price)
+
+            self.portfolio_history.record_cash(date, self.cash)
+
+        for s in self.active_symbols:
+            self.sell_to_close(s, date, row[_idx(s, 'price')])
+        self.portfolio_history.finish()
